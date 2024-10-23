067326ab29abc22928985669cfe90eb9316db9f9
Chris <christtopherluna@gmail.com>
2024-10-22 21:33:24 -0400

    started building out classes for items in file

---
+++ VendingMachine/Chip.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    internal class Chip : Product
+    {
+        public Chip(string productName, decimal price, int quantity, string slotNumber) : base(productName, price, quantity, slotNumber)
+        {
+        }
+
+
+    }
+}
---
+++ VendingMachine/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string SlotNumber { get; set; }
+
+        public Product(string productName, decimal price, int quantity, string slotNumber)
+        {
+            ProductName = productName;
+            Price = price;
+            Quantity = quantity;
+            SlotNumber = slotNumber;
+        }
+
+        public string GetProductName()
+        {
+            return ProductName;
+        }
+        public decimal GetPrice()
+        {
+            return Price;
+        }
+
+        public int GetQuantity() 
+        {
+            return Quantity;
+        }
+
+        public string GetSlotNumber() 
+        {
+            return SlotNumber;
+        }
+    }
+}
